fb98874b7ea7f6f19d1aecc10c4dc3510d56faec
lopetegui_h@hotmail.com <lopetegui_h@hotmail.com>
2021-10-23 23:24:06 -0300

    Vercion inicial

---
+++ Javascript desde Cero Dlto/JS/js.js
@@ -0,0 +1,3 @@
+texto = "23"
+texto2 =" texto 2"
+document.write(texto==texto2)
---
+++ Javascript desde Cero Dlto/ejercicios/Capitulo 1/js.js
@@ -0,0 +1,5 @@
+prompt("Cuanto dinero tiene 1: ")
+prompt("Cuanto dinero tiene 2: ")
+prompt("Cuanto dinero tiene 3: ")
+
+
