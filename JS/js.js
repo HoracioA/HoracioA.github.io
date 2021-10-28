c03ddde277424f0d2e9697588d3e8638ca87c51b
lopetegui_h@hotmail.com <lopetegui_h@hotmail.com>
2021-10-28 01:14:03 -0300

    Paguina echa

---
+++ JS/js.js
@@ -0,0 +1,52 @@
+var e1 = document.getElementById('div1');
+e1.onmouseover = function() {
+  document.getElementById('info1').style.display = 'block';
+}
+e1.onmouseout = function() {
+  document.getElementById('info1').style.display = 'none';
+}
+
+
+var e2 = document.getElementById('div2');
+e2.onmouseover = function() {
+  document.getElementById('info2').style.display = 'block';
+}
+e2.onmouseout = function() {
+  document.getElementById('info2').style.display = 'none';
+}
+
+
+var e3 = document.getElementById('div3');
+e3.onmouseover = function() {
+  document.getElementById('info3').style.display = 'block';
+}
+e3.onmouseout = function() {
+  document.getElementById('info3').style.display = 'none';
+}
+
+
+var e4 = document.getElementById('div4');
+e4.onmouseover = function() {
+  document.getElementById('info4').style.display = 'block';
+}
+e4.onmouseout = function() {
+  document.getElementById('info4').style.display = 'none';
+}
+
+
+var e5 = document.getElementById('div5');
+e5.onmouseover = function() {
+  document.getElementById('info5').style.display = 'block';
+}
+e5.onmouseout = function() {
+  document.getElementById('info5').style.display = 'none';
+}
+
+
+var e6 = document.getElementById('div6');
+e6.onmouseover = function() {
+  document.getElementById('info6').style.display = 'block';
+}
+e6.onmouseout = function() {
+  document.getElementById('info6').style.display = 'none';
+}
